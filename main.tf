fdf388404b204caa490d0a4ebe5ac2650a747757
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-02 12:42:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,27 @@ module "s3_bucket" {
 module "lambda_func" {
   source = "git::https://github.com/wilshan/terraform-aws-lambda.git"
   function_name = var.function_name
+  lambda_handler = var.lambda_handler
+  runtime = var.runtime
+  lambda_execution_role = var.lambda_execution_role
+  s3_bucket = var.s3_bucket
+  s3_key = var.s3_key
+  cwrulename = var.cwrulename
+  schedule = var.schedule
+  target_id = var.target_id
+  statement_id2 = var.statement_id2
+  action2 = var.action2
+  principle2 = var.principle2
+  event_bucket_name = var.event_bucket_name
+  event_type = var.event_type
+  prefix = var.prefix
+  suffix = var.suffix
+  statement_id1 = var.statement_id1
+  action1 = var.action1
+  principle1 = var.principle1
+  source_arn = var.source_arn
+}
+  
   
   
   
