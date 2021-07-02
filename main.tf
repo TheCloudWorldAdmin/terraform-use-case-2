232d41cfe70abbe68a435c27fad8fc6fd655ed1e
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-02 12:16:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,14 @@
+module "s3_bucket" {
+  source = "git::https://github.com/wilshan/terraform-aws-s3.git"
+  bucketname = var.bucketname
+  acl = var.acl
+  env = var.env
+  versioning_enabled = var.versioning_enabled
+}
 
+module "lambda_func" {
+  source = "git::https://github.com/wilshan/terraform-aws-lambda.git"
+  function_name = var.function_name
+  
+  
+  
