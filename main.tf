1f83a1c92f915a627f3ef49463a5b93806227528
wilshan <74950445+wilshan@users.noreply.github.com>
2021-07-02 13:36:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,22 +12,15 @@ module "lambda_func" {
   lambda_handler = var.lambda_handler
   runtime = var.runtime
   lambda_execution_role = var.lambda_execution_role
-  s3_bucket = var.s3_bucket
+  s3_bucket = module.s3_bucket.bucketname
   s3_key = var.s3_key
   cwrulename = var.cwrulename
-  schedule = var.schedule
-  target_id = var.target_id
-  statement_id2 = var.statement_id2
-  action2 = var.action2
-  principle2 = var.principle2
   event_bucket_name = var.event_bucket_name
   event_type = var.event_type
-  prefix = var.prefix
-  suffix = var.suffix
   statement_id1 = var.statement_id1
   action1 = var.action1
   principle1 = var.principle1
-  source_arn = var.source_arn
+  source_arn = module.s3_bucket.arn
 }
   
   
